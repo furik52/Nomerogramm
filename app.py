c8a7fb0c8f2a1d1e100140194967a057ec8fb40e
learless <167815614+learless@users.noreply.github.com>
2025-03-13 12:28:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from customtkinter import *
+import cv2
+import numpy as np
 
 #dark/light mode + default objects color theme
 set_appearance_mode('dark')
@@ -11,6 +13,8 @@ root.geometry('800x600')
 def click_handler():
     input_file = filedialog.askopenfile().name
     print('Путь до файла:', input_file)
+    image = cv2.imdecode(np.fromfile(input_file, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+    cv2.imshow("OpenCV", image)
 
 btn = CTkButton(master=root, text='Выберите фотографию', corner_radius=16, command=click_handler)
 btn.place(relx=0.5, rely=0.5, anchor='center')
