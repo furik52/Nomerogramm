e4cd90dda8f5e6362aed869f528a01d7d8d9e6d9
furik52 <vladfurov41@gmail.com>
2025-03-14 21:14:33 +0300

    add nomerogramm.py

---
+++ nomerogramm.py
@@ -0,0 +1,52 @@
+from customtkinter import *
+import cv2
+import numpy as np
+
+def main(path_to_file, out_size = 28):
+    print('Путь до файла:', path_to_file)
+    image = cv2.imdecode(np.fromfile(path_to_file, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+    cv2.imshow("OpenCV", image)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY)
+    img_erode = cv2.erode(thresh, np.ones((3, 3), np.uint8), iterations=1)
+
+    contours, hierarchy = cv2.findContours(img_erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    output = image.copy()
+    letters = []
+    for idx, contour in enumerate(contours):
+        (x, y, w, h) = cv2.boundingRect(contour)
+        if hierarchy[0][idx][3] == 0:
+            cv2.rectangle(output, (x, y), (x + w, y + h), (70, 0, 0), 1)
+            letter_crop = gray_image[y:y + h, x:x + w]
+            # print(letter_crop.shape)
+
+            # Resize letter canvas to square
+            size_max = max(w, h)
+            letter_square = 255 * np.ones(shape=[size_max, size_max], dtype=np.uint8)
+            if w > h:
+                # Enlarge image top-bottom
+                # ------
+                # ======
+                # ------
+                y_pos = size_max//2 - h//2
+                letter_square[y_pos:y_pos + h, 0:w] = letter_crop
+            elif w < h:
+                # Enlarge image left-right
+                # --||--
+                x_pos = size_max//2 - w//2
+                letter_square[0:h, x_pos:x_pos + w] = letter_crop
+            else:
+                letter_square = letter_crop
+
+            # Resize letter to 28x28 and add letter and its X-coordinate
+            letters.append((x, w, cv2.resize(letter_square, (out_size, out_size), interpolation=cv2.INTER_AREA)))
+
+    # Sort array in place by X-coordinate
+            letters.sort(key=lambda x: x[0], reverse=False)
+    print(len(letters))
+    '''cv2.imshow("0", letters[0][1])
+    cv2.imshow("1", letters[1][2])
+    cv2.imshow("2", letters[2][2])
+    cv2.imshow("3", letters[3][2])
+    cv2.imshow("4", letters[4][2])'''
+    cv2.waitKey(0)
