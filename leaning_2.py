6f24be4c576acf04037250fac53cb8ace8a76882
furik52 <vladfurov41@gmail.com>
2025-03-30 10:14:59 +0300

    Update learning_2.py

--- leaning_2.py
+++ leaning_2.py
@@ -1,5 +1,13 @@
 import tensorflow_datasets as tfds
+import os
 import tensorflow as tf
-ds = tfds.load('emnist', split='train', shuffle_files=True)
+
+allowed_labels = [*range(10), 10, 11, 12, 14, 17, 22, 24, 25, 30, 31, 33 ]
+
+def filter_fn(example):
+    return tf.reduce_any(example['label'] == allowed_labels)
+
+ds = tfds.load('emnist/byclass', split='train[::5]', data_dir=os.getcwd(),shuffle_files=True).filter(filter_fn)
+
 assert isinstance(ds, tf.data.Dataset)
 print(ds)
