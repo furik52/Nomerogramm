85a7afa7920850f864e16a991f1b89ec2000d5ac
learless <167815614+learless@users.noreply.github.com>
2025-04-02 15:03:12 +0300

    micro changes

--- leaning_2.py
+++ leaning_2.py
@@ -7,7 +7,7 @@ allowed_labels = [*range(10), 10, 11, 12, 14, 17, 22, 24, 25, 30, 31, 33 ]
 def filter_fn(example):
     return tf.reduce_any(example['label'] == allowed_labels)
 
-ds = tfds.load('emnist/byclass', split='train[::5]', data_dir=os.getcwd(),shuffle_files=True).filter(filter_fn)
+ds = tfds.load('emnist/byclass', split='train[:20%]', data_dir=os.getcwd(),shuffle_files=True).filter(filter_fn)
 
 assert isinstance(ds, tf.data.Dataset)
 print(ds)
